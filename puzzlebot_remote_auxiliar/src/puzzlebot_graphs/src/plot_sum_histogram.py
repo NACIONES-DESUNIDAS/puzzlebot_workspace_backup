024d4d588ecaf051a70b4e07a7cc801d955ad9e8
luis <dariophoskyhinojosa@gmail.com>
2022-05-30 18:08:34 -0500

    e

--- puzzlebot_remote_auxiliar/src/puzzlebot_graphs/src/plot_sum_histogram.py
+++ puzzlebot_remote_auxiliar/src/puzzlebot_graphs/src/plot_sum_histogram.py
@@ -29,11 +29,10 @@ class Puzzlebot_Grapher:
 
 
     # split the vertical sum into two different arrays based on right and left edges
-    def splitEdges(self,gradient,scaleFactor = 0.2):
-        min = np.min(gradient) * scaleFactor
-        max = np.max(gradient) * scaleFactor
+    def splitEdges(self,gradient,maxScaleFactor = 0.2,minScaleFactor = 0.2):
+        min = np.min(gradient) * minScaleFactor
+        max = np.max(gradient) * maxScaleFactor
         mean = np.mean(gradient)
-        #rospy.loginfo(mean)
         leftEdges = gradient.copy()
         rightEdges = gradient.copy()
         leftEdges[leftEdges > max ] = mean
@@ -43,8 +42,18 @@ class Puzzlebot_Grapher:
         return leftEdges,rightEdges
 
 
+    def filterWithNumber(self,gradient,threshold = 20,up=True):
+        filtered = gradient.copy()
+        if up:
+            filtered[filtered < threshold] = 0
+        else:
+            filtered[filtered > -1*threshold] = 0
+
+        return filtered
+
+
     # filter and array to eliminate noise
-    def filterWithThreshold(self,gradient,scaleThreshold = 0.3):
+    def filterWithThreshold(self,gradient,scaleThreshold = 0.4):
         min = np.min(gradient) * scaleThreshold
         max = np.max(gradient) * scaleThreshold
         positive = gradient.copy()
@@ -67,47 +76,97 @@ class Puzzlebot_Grapher:
         compare = shifted > gradient
         return compare
 
+    def cleanSignal(self,edges,thres = 10):
+        edgeList = list(edges[0])
+        cleanEdges = list()
+        edge = edgeList[0]
+        cleanEdges.append(edge)
+        for i in range(1,len(edgeList)):
+            diff = abs(edgeList[i]-edge)
+            if diff < thres:
+                continue
+            else:
+                edge = edgeList[i]
+                cleanEdges.append(edge)
+        return np.array(cleanEdges)
+        
+    def mapEdges(self,leftEdges,rightEdges, minWidth = 40, maxWidth = 70):
+        #rospy.loginfo(len(leftEdges))
+        #rospy.loginfo(len(rightEdges))
+
+        if len(leftEdges) > 0 and len(rightEdges) > 0:
+            mapp = list()
+            for i in range(len(leftEdges)):
+                for j in range(len(rightEdges)):
+                    diff = abs(leftEdges[i] - rightEdges[j])
+                    if diff > minWidth and diff < maxWidth and rightEdges[j] != None:
+                        mapp.append((leftEdges[i],rightEdges[j]))
+                        rightEdges[j] = -1
+
+            return mapp
+        else:
+            return None
+
     def run(self):
         plt.figure()
         while not rospy.is_shutdown():
             if self.array is None:
                 self.rate.sleep()
-            #= np.uint64(self.array)
+
             # compute gradient
             gradient = np.gradient(self.array)
+
             # split the left edges and the right edges
             left,right = self.splitEdges(gradient)
+
+
+
             # compute second gradient
             secondLeftGradient = np.gradient(left)
             secondRightGradient = np.gradient(right)
+
             # filter noise from second gradient
             secondLeftGradient = self.filterWithThreshold(secondLeftGradient)
             secondRightGradient = self.filterWithThreshold(secondRightGradient)
+
             # mutiply first with second gradient
             leftMul = left * secondLeftGradient
             rightMul = right * secondRightGradient
+
             # remove negative portion of both arrays
             leftPositive = self.filterNegative(leftMul)
             rightPositive = self.filterNegative(rightMul)
+
             # pixelShift:
             leftCompare = self.shiftCompare(leftPositive)
             rightCompare = self.shiftCompare(rightPositive)
+
             # right and left slices positions
             leftEdges = np.where(leftCompare)
             rightEdges = np.where(rightCompare)
 
+            # clean from repeated and proximal edges
+            leftEdges = self.cleanSignal(leftEdges)
+            rightEdges = self.cleanSignal(rightEdges)
+
+            rospy.loginfo("Left Edges:")
             rospy.loginfo(leftEdges)
+            rospy.loginfo("Right Edges:")
             rospy.loginfo(rightEdges)
 
-            plt.subplot(3,1,1)
+
+
+            plt.subplot(2,2,1)
+            plt.plot(gradient)
+            plt.subplot(2,2,2)
             plt.plot(left)
             plt.plot(right)
             plt.legend(["left","right"])   
-            plt.subplot(3,1,2)
+            plt.subplot(2,2,3)
             plt.plot(secondLeftGradient)
             plt.plot(secondRightGradient)
             plt.legend(["left","right"])   
-            plt.subplot(3,1,3)
+            plt.subplot(2,2,4)
             plt.plot(leftPositive)
             plt.plot(rightPositive)
             plt.legend(["left","right"])            
